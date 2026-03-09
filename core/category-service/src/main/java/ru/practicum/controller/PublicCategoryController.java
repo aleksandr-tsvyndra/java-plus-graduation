2fdbe84a064a72d5a60a7f3487975bf124c72539
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2026-03-09 20:13:25 +0300

    Исправить pom-файл в interaction-api: добавить тег skip со значением true в конфигурации плагина spring-boot-maven-plugin

--- core/category-service/src/main/java/ru/practicum/controller/PublicCategoryController.java
+++ core/category-service/src/main/java/ru/practicum/controller/PublicCategoryController.java
@@ -1,12 +1,9 @@
 package ru.practicum.controller;
 
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.api.category.PublicCategoryApi;
 import ru.practicum.dto.category.CategoryDto;
 import ru.practicum.service.CategoryService;
 
@@ -14,17 +11,16 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class PublicCategoryController {
+public class PublicCategoryController implements PublicCategoryApi {
     private final CategoryService categoryService;
 
-    @GetMapping
-    public List<CategoryDto> getCategories(@RequestParam(defaultValue = "0") int from,
-                                           @RequestParam(defaultValue = "10") int size) {
+    @Override
+    public List<CategoryDto> getCategories(int from, int size) {
         return categoryService.getCategories(PageRequest.of(from / size, size));
     }
 
-    @GetMapping("/{catId}")
-    public CategoryDto getCategoryById(@PathVariable @Positive Long catId) {
+    @Override
+    public CategoryDto getCategoryById(Long catId) {
         return categoryService.getCategoryById(catId);
     }
 }
