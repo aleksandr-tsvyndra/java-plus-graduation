67b017fda45bdc5a6b5f2f518ddf1e4b09897d36
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2026-03-14 23:09:19 +0300

    Тестовый коммит

--- stats/telemetry/aggregator/src/main/java/ru/practicum/Aggregator.java
+++ stats/telemetry/aggregator/src/main/java/ru/practicum/Aggregator.java
@@ -4,20 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Aggregator {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
-        AggregationStarter aggregationStarter = context.getBean(AggregationStarter.class);
-
-        Runtime.getRuntime().addShutdownHook(new Thread(aggregationStarter::stop));
-
-        Thread aggregatorThread = new Thread(aggregationStarter::start);
-        aggregatorThread.setName("AggregatorThread");
-        aggregatorThread.start();
+        SpringApplication.run(Aggregator.class, args);
     }
 }
